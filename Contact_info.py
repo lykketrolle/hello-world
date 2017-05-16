6b8094758a9bff0b2354ee6c80260b47289f6f11
lykketrolle <chr.larsen69@outlook.com>
2017-05-16 14:31:59 +0200

    Add files via upload

---
+++ Contact_info.py
@@ -0,0 +1,13 @@
+
+name = input("Please enter your first and last name: ")
+street = input("Please enter your street name and number. ")
+zip_code = input("Please enter your ZIP-code: ")
+state = input("Please enter your state: ")
+phone = int(input("Please enter your phone number: "))
+email = input("Please enter your Email address: ")
+
+print("Name: ", name)
+print("Street: ", street)
+print("Zip-code: ", zip_code, "State: ", state)
+print("Phone: ", phone)
+print("Email address: ", email)
